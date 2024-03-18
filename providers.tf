0776b04b1de3f18bf03fa8227f2a7a0717d4fb3e
“Samuel <samuel.lotfi@property.nhs.uk>
2024-03-18 10:40:30 +0000

    create rs

--- providers.tf
+++ providers.tf
@@ -13,6 +13,10 @@ terraform {
 
 provider "azurerm" {
   features {}
+    subscription_id = "7617f663-628c-4ada-9e64-f1f060e2de9b" # my sub id
+    tenant_id       = "9974da4c-818b-466c-8bd3-0c49e5467345" # my tenant id
+    #client_id         = "fd9e9497-e0ca-40f3-9e5f-0be13c520917"
+    #client_secret     = "<service_principal_password>"
 
-  
+
 }
